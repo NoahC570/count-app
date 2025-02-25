9bd818eeb736b0c19f1d6a7850f5345c1c0cbaf7
NoahC570 <noah215557@gmail.com>
2025-02-24 20:59:03 -0500

    Conf not wokring ?

--- count-app.js
+++ count-app.js
@@ -49,6 +49,38 @@ export class CountApp extends DDDSuper(I18NMixin(LitElement)) {
     };
   }
 
+  updated(changedProperties) {
+    if (super.updated) {
+      super.updated(changedProperties);
+    }
+    if (changedProperties.has('count')) {
+      if (this.count === 21) {
+        this.makeItRain();
+      }
+      // do your testing of the value and make it rain by calling makeItRain
+    }
+  }
+
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
+  }
+
   // Lit scoped styles
   static get styles() {
     return [super.styles,
@@ -59,10 +91,10 @@ export class CountApp extends DDDSuper(I18NMixin(LitElement)) {
         background-color: var(--ddd-theme-accent);
         font-family: var(--ddd-font-navigation);
       }
-      :host([count = "18" ]) {
+      :host([count="18"]) {
         color: var(--ddd-theme-default-athertonViolet);
       }
-      :host([count = "21" ]) {
+      :host([count="21"]) {
         color: var(--ddd-theme-default-keystoneYellow);
       }
       .wrapper {
@@ -79,11 +111,13 @@ export class CountApp extends DDDSuper(I18NMixin(LitElement)) {
   render() {
     return html`
       <div class="wrapper">
-      <div class="counter">${this.count}</div>
-      <div class="buttons">
-        <button @click = "${this.decrese}" ?disabled="${this.min === this.count}">-1</button>
-        <button @click = "${this.increase}" ?disabled="${this.max === this.count}">+1</button>
-    </div>    
+        <confetti-container id="confetti"></confetti-container>
+        <div class="counter">${this.count}</div>
+        <div class="buttons">
+          <button @click = "${this.decrease}" ?disabled="${this.min === this.count}">-1</button>
+          <button @click = "${this.increase}" ?disabled="${this.max === this.count}">+1</button>
+        </div>    
+      </div>
     `;
   }
 
@@ -93,7 +127,7 @@ export class CountApp extends DDDSuper(I18NMixin(LitElement)) {
     }
   }
 
-  decrese() {
+  decrease() {
     if (this.count > this.min) {
       this.count--;
     } 
